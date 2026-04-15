dfb5a3c2c4fd0b47e73b4932f6976cc48c39eae2
ronaksharma-simform <ronak.sharma@simformsolutions.com>
2026-04-15 18:09:08 +0530

    chore: create basic folder structure with eslint and prettier format

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from "express";
 
 const app = express();
-import cors from "cors";
+// import cors from "cors";
+// let temp;
 // enables cors
 // app.use(cors());
 // testing route
-app.get("./", (req, res) => {
-  res.send("Working");
+app.get("/temp", (req, res) => {
+  res.json("Working");
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
+dotenv.config();
 import dotenv from "dotenv";
 import app from "./app";
-dotenv.config();
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server running on port " + process.env.PORT);
 });
